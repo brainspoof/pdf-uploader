54216887e37d91d12f274220d99d984fc02901bb
Shubham Gupta <shubhastro2@gmail.com>
2020-06-04 22:08:40 +0530

    Dynamic PDF Storage

--- pdfuploader/main/models.py
+++ pdfuploader/main/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+def get_path(instance, filename):
+    return f'pdfs/{instance.user_email}/{filename}'
+
 class PDF(models.Model):
     user_email = models.EmailField()
     secret = models.CharField(max_length=500)
-    pdf = models.FileField(upload_to='pdfs/')
+    pdf = models.FileField(upload_to=get_path)
 
     def __str__(self):
         return f'PDF of {self.user_email}.'
